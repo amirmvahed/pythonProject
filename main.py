c0c0c30c353a3ff8140f4873610d072b80946258
amirmvahed <amirmvahed@gmail.com>
2021-10-01 13:29:08 +0330

    Numbers

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-# use_full string methods
+# Use_ful string methods
 
-course = '  Python Programing  '
-print(course.upper())
-print(course.lower())
-print(course.title())
+# course = '  Python Programing  '
+# print(course.upper())
+# print(course.lower())
+# print(course.title())
+#
+# print(course.strip())
+# print(course.lstrip())
+# print(course.rstrip())
+#
+# print(course.find("Python"))
+# print(course.replace("P", '-'))
+#
+# print('Programing' in course)
+# print('Programing' not in course)
 
-print(course.strip())
-print(course.lstrip())
-print(course.rstrip())
 
-print(course.find("Python"))
-print(course.replace("P", '-'))
+# Numbers
+# x = 10
 
-print('Programing' in course)
-print('Programing' not in course)
+# binary
+# b = 0b10
+# print(b)
+# print(bin(x))
+
+# hexadecimal
+# c = 0x12c
+# print(c)
+# print(hex(x))
+
+# complex_number a + bj
+# d = 1 + 2j
+# print(d)
+

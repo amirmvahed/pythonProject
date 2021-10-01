fb2fd84baa968342038d9a599e60c68d0bee46ae
amirmvahed <amirmvahed@gmail.com>
2021-10-01 13:21:02 +0330

    usefull string methods

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print('Hello World')
+# use_full string methods
+
+course = '  Python Programing  '
+print(course.upper())
+print(course.lower())
+print(course.title())
+
+print(course.strip())
+print(course.lstrip())
+print(course.rstrip())
+
+print(course.find("Python"))
+print(course.replace("P", '-'))
+
+print('Programing' in course)
+print('Programing' not in course)

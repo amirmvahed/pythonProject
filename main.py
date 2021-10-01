dde1e129314e018d21fc6568f8c8e119abb71308
amirmvahed <amirmvahed@gmail.com>
2021-10-01 16:17:29 +0330

    Loops

--- main.py
+++ main.py
@@ -107,3 +107,49 @@
 # age = 22
 # message = 'eligible' if age > 18 else 'not eligible'
 # print(message)
+
+# Loops
+
+for x in 'Python':
+    print(x)
+
+for x in ['a', 'b', 'c']:
+    print('x')
+
+for x in range(5):
+    print(x)
+for x in range(2, 5):
+    print(x)
+#             start, end, step
+for x in range(1, 10, 2):
+    print(x)
+# range is not a list try type() function
+
+# for - else
+names = ["John", "Mosh"]
+for name in names:
+    if name.startswith('J'):
+        print('Found')
+        break
+else:
+    print('not Found')
+
+# instead of
+found = False
+for name in names:
+    if name.startswith('J'):
+        print('Found')
+        found = True
+        break
+if not found:
+    print('not Found')
+
+# while
+
+guess = 0
+current = 3
+
+while guess != 3:
+    guess = int(input('guess: '))
+else:
+    pass

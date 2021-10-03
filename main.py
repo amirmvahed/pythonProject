42ec20b8f9cf1c6c398a333d235af545dc530a6d
amirmvahed <amirmvahed@gmail.com>
2021-10-03 19:45:23 +0330

    Lists

---
+++ main.py
@@ -0,0 +1,8 @@
+# Lists
+# letters = ['a', 'b', 'c']
+# zeros = [0] * 5
+# matrix = [[1, 2], [0, 5]]
+# numbers = list(range(20))
+# combine = zeros + letters
+# chars = list('Hello World')
+# print(chars)

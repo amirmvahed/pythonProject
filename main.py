2939f0f174d202cdcaf0e9f16d305df0d7ca6c82
amirmvahed <amirmvahed@gmail.com>
2021-10-03 19:56:47 +0330

    Accessing to item of Lists

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 # Lists
-# letters = ['a', 'b', 'c']
+# letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
 # zeros = [0] * 5
 # matrix = [[1, 2], [0, 5]]
 # numbers = list(range(20))
 # combine = zeros + letters
 # chars = list('Hello World')
 # print(chars)
+# print(letters[2])
+# print(letters[0:1])
+# print(letters[0:4])
+# print(letters[0:7:2])
+# print(letters[::2])
+# print(letters[1:])
+# print(letters[:3])
